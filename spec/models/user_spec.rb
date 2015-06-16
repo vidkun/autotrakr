9582ea5874361c9841f5179d7b5b0684f76e78ee
Ryan LeViseur <vidkun@users.noreply.github.com>
2015-06-16 18:42:29 -0500

    add some tests...

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,21 @@ require 'rails_helper'
  
 describe User do
  
+  it { is_expected.to have_fields(:username, :email, :first_name, :last_name, :password_digest) }
+  it { is_expected.to validate_presence_of(:username) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive.with_message("is already taken") }
+  it { is_expected.to validate_uniqueness_of(:username).case_insensitive.with_message("is already taken") }
+
   it 'fails because no passwrod' do
     expect( User.new( { username: 'bill' } ).save ).to be false
   end
  
-  it 'fails because passwrod to short' do
-    expect( User.new( { username: 'bill', password: 'ted' } ).save ).to be false
-  end
+  it { is_expected.to validate_length_of(:password).greater_than(8) }
+  #it 'fails because passwrod to short' do
+    #expect( User.new( { username: 'bill', password: 'ted' } ).save ).to be false
+  #end
  
   it 'succeeds because password is long enough' do
     expect( User.new( { username: 'bill', password: 'wyldstallyns' } ).save ).to be true
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,6 +39,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.include FactoryGirl::Syntax::Methods
+  config.include Mongoid::Matchers, type: :model
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = false
   config.before(:suite) do
