7d38729c38d893583e30131f541a6339221df2ca
Ryan LeViseur <vidkun@users.noreply.github.com>
2015-06-16 16:55:32 -0500

    add has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User
   include Mongoid::Document
+  include ActiveModel::SecurePassword
   field :username, type: String
   field :email, type: String
   field :first_name, type: String
   field :last_name, type: String
   field :password_digest, type: String
+  has_secure_password
 end
