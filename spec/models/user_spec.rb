29b5304d5bdaa94ca7bd319d41c297aaddcc3fbb
Ryan LeViseur <vidkun@users.noreply.github.com>
2015-06-16 18:29:02 -0500

    moar excellent tests for user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,12 @@ describe User do
     expect( User.new( { username: 'bill', password: 'wyldstallyns' } ).save ).to be true
   end
  
+  it 'fails because password confirmation does not match' do
+    expect( User.new( { username: 'bill', password: 'wyldstallyns', password_confirmation: 'abc123' } ).save ).to be false
+  end
+ 
+  it 'succeeds because password & confirmation match' do
+    expect( User.new( { username: 'bill', password: 'wyldstallyns', password_confirmation: 'wyldstallyns' } ).save ).to be true
+  end
+
 end
