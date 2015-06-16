956b803785deb29973976c334012341f731c8f93
Ryan LeViseur <vidkun@users.noreply.github.com>
2015-06-16 18:23:37 -0500

    add password length validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User
   include Mongoid::Document
   include ActiveModel::SecurePassword
+
   field :username, type: String
   field :email, type: String
   field :first_name, type: String
   field :last_name, type: String
   field :password_digest, type: String
+
   has_secure_password
+
+  validates :password, length: { minimum: 8 }
 end
