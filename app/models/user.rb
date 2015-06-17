ad94b6d59c2c9e874d8302d8f39e8f155ad997b1
Ryan LeViseur <vidkun@users.noreply.github.com>
2015-06-16 19:57:54 -0500

    clean up code. got tests passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,11 @@ class User
   has_secure_password
 
   validates :password, length: { minimum: 8 }
+  validates_presence_of :username,
+                        :email,
+                        :password
+  validates_uniqueness_of :username,
+                          :email,
+                          case_sensitive: false,
+                          message: 'is already taken'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,32 +2,50 @@ require 'rails_helper'
  
 describe User do
  
-  it { is_expected.to have_fields(:username, :email, :first_name, :last_name, :password_digest) }
+  it { is_expected.to have_fields(:username,
+                                  :email,
+                                  :first_name,
+                                  :last_name,
+                                  :password_digest) }
   it { is_expected.to validate_presence_of(:username) }
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_presence_of(:password) }
-  it { is_expected.to validate_uniqueness_of(:email).case_insensitive.with_message("is already taken") }
-  it { is_expected.to validate_uniqueness_of(:username).case_insensitive.with_message("is already taken") }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive
+                                                    .with_message('is already taken') }
+  it { is_expected.to validate_uniqueness_of(:username).case_insensitive
+                                                       .with_message('is already taken') }
 
   it 'fails because no passwrod' do
     expect( User.new( { username: 'bill' } ).save ).to be false
   end
  
-  it { is_expected.to validate_length_of(:password).greater_than(8) }
-  #it 'fails because passwrod to short' do
-    #expect( User.new( { username: 'bill', password: 'ted' } ).save ).to be false
-  #end
+  #it { is_expected.to validate_length_of(:password).with_minimum(8) }
+  it 'fails because passwrod to short' do
+    expect( User.new( { username: 'bill',
+                        email: 'bill@wyldstallyns.com',
+                        password: 'ted',
+                        password_confirmation: 'ted' } ).save ).to be false
+  end
  
   it 'succeeds because password is long enough' do
-    expect( User.new( { username: 'bill', password: 'wyldstallyns' } ).save ).to be true
+    expect( User.new( { username: 'bill',
+                        email: 'bill@wyldstallyns.com',
+                        password: 'wyldstallyns',
+                        password_confirmation: 'wyldstallyns' } ).save ).to be true
   end
  
   it 'fails because password confirmation does not match' do
-    expect( User.new( { username: 'bill', password: 'wyldstallyns', password_confirmation: 'abc123' } ).save ).to be false
+    expect( User.new( { username: 'bill',
+                        email: 'bill@wyldstallyns.com',
+                        password: 'wyldstallyns',
+                        password_confirmation: 'abc123' } ).save ).to be false
   end
  
   it 'succeeds because password & confirmation match' do
-    expect( User.new( { username: 'bill', password: 'wyldstallyns', password_confirmation: 'wyldstallyns' } ).save ).to be true
+    expect( User.new( { username: 'bill',
+                        email: 'bill@wyldstallyns.com',
+                        password: 'wyldstallyns',
+                        password_confirmation: 'wyldstallyns' } ).save ).to be true
   end
 
 end
