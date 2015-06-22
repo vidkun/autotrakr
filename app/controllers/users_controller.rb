74066fdaa9f2d31f491768470f9ace423cf9be91
Ryan LeViseur <vidkun@users.noreply.github.com>
2015-06-21 20:04:34 -0500

    fix user redirects, clean up registration feature spec, and style user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to users_url,
+      redirect_to user_url(@user),
                   notice: "User #{@user.username} was successfully created."
     else
       render :new
--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -4,16 +4,19 @@ describe 'Signing up' do
   it 'allows a user to sign up for the site and creates the object in the database' do
     expect(User.count).to eq(0)
 
+    user = build(:user, :valid_password)
+    
     visit '/'
     expect(page).to have_content('Sign Up')
     click_link 'Sign Up'
 
-    fill_in 'First Name', with: 'Bill'
-    fill_in 'Last Name', with: 'Preston'
-    fill_in 'Email', with: 'bpreston@wyldstallyns.com'
-    fill_in 'Password', with: 'Excellent!'
-    fill_in 'Confirm Password', with: 'Excellent!'
-    click_button 'Sign Up'
+    fill_in 'Username', with: user.username
+    fill_in 'First Name', with: user.first_name
+    fill_in 'Last Name', with: user.last_name
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Confirm Password', with: user.password_confirmation
+    click_button 'Register'
 
     expect(User.count).to eq(1)
   end
