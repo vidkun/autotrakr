6514d4f9f7ee9af692e0f39ccb71d76755bbc6ab
Ryan LeViseur <vidkun@users.noreply.github.com>
2015-06-27 18:04:36 -0500

    add another feature spec to authentication_spec for successfull login

--- spec/features/users/authentication_spec.rb
+++ spec/features/users/authentication_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 describe "Logging In" do
+  it "logs the user in and goes to the user page" do
+    user = create(:user, :valid_password)
+
+    visit login_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log In"
+
+    expect(page).to have_content("Login Successful")
+  end
+
   it "displays the email address in the event of a failed login" do
     user = create(:user, :valid_password)
 
