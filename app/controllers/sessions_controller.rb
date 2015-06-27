68f82971d2c1e438cc58481e38d48f9628eea754
Ryan LeViseur <vidkun@users.noreply.github.com>
2015-06-27 13:41:20 -0500

    build out basic session create actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect_to user_path(user)
   end
 
   def destroy
   end
+  
 end
