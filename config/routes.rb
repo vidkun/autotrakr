08fdb7d3d90db52920d93e866dc1bea48d0cc3c0
Ryan LeViseur <vidkun@users.noreply.github.com>
2015-07-02 13:20:40 -0500

    implement password reset notifier, specs, and action for when a user exists and is valid

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :users, except: [:index, :destroy]
   get 'signup', to: 'users#new', as: 'signup'
 
-  resources :password_resets, only: [:new, :create]
+  resources :password_resets, only: [:new, :create, :edit]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
