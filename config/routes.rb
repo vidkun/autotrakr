39271cb011db83dd85b3ec450532bae8c56b6327
Ryan LeViseur <vidkun@users.noreply.github.com>
2015-07-02 13:01:10 -0500

    add more password reset examples and begin implementing password reset create action

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   resources :users, except: [:index, :destroy]
   get 'signup', to: 'users#new', as: 'signup'
 
-  resources :password_resets, only: [:new]
-  
+  resources :password_resets, only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
