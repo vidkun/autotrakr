b44251523d759b3d2ca6800764355e4dc8a40fb8
Ryan LeViseur <vidkun@users.noreply.github.com>
2015-08-31 22:26:36 -0500

    fix vehicles factory and add some specs for vehicle model

--- spec/factories/vehicles.rb
+++ spec/factories/vehicles.rb
@@ -1,15 +1,15 @@
 FactoryGirl.define do
   factory :vehicle do
-    year          { Faker::Number.number(4) }
+    year          { Faker::Number.number(4).to_i }
     make          { Faker::Company.name }
-    model         { Faker::Lorem.words }
-    engine        { Faker::Lorem.words }
+    model         { Faker::Lorem.words.to_s }
+    engine        { Faker::Lorem.words.to_s }
     transmission  { Faker::Lorem.word }
     drive         { Faker::Lorem.word }
     fuel          { Faker::Lorem.word }
     mileage       { Faker::Number.number(6) }
     color         { Faker::Commerce.color }
-    user          { create(:user, :valid_password) }
+    association :user, :valid_password
   end
 
 end
