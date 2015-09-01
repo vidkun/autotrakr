70da54998ec5c529078167ca0be888edd823e22f
Ryan LeViseur <vidkun@users.noreply.github.com>
2015-09-01 17:03:20 -0500

    fix vehicles factory to create proper multi-word values

--- spec/factories/vehicles.rb
+++ spec/factories/vehicles.rb
@@ -2,12 +2,12 @@ FactoryGirl.define do
   factory :vehicle do
     year          { Faker::Number.number(4).to_i }
     make          { Faker::Company.name }
-    model         { Faker::Lorem.words.to_s }
-    engine        { Faker::Lorem.words.to_s }
+    model         { Faker::Lorem.words.join(' ') }
+    engine        { Faker::Lorem.words.join(' ') }
     transmission  { Faker::Lorem.word }
     drive         { Faker::Lorem.word }
     fuel          { Faker::Lorem.word }
-    mileage       { Faker::Number.number(6) }
+    mileage       { Faker::Number.number(6).to_i }
     color         { Faker::Commerce.color }
     association :user, :valid_password
   end
