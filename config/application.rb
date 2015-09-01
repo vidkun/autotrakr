e671e0197efb3a4963d98a136d3d8ce666b4abc4
Ryan LeViseur <vidkun@users.noreply.github.com>
2015-08-31 19:20:15 -0500

    remove mongoid generator config

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Autotrakr
     # config.i18n.default_locale = :de
 
     config.generators do |g|
-        g.orm             :mongoid
+        # g.orm             :nobrainer
         # g.template_engine :haml
         g.test_framework  :rspec, :fixture => false
         g.view_specs      false
